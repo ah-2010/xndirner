ef1c407f7c657f163ff253bf7c7bf6c55cf6d4b3
ah-2010 <138315547+ah-2010@users.noreply.github.com>
2023-11-08 20:26:17 +0400

    I did my 6 problems

--- xndirner/src/Xndirner.java
+++ xndirner/src/Xndirner.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Xndirner {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
        MekIcHarur();
        DoMekIcHarur();
         zuygTver();
+        DoZuygTver();
+        factorial(number);
+        primeNumber(number);
     }
 
     public static void MekIcHarur() {
@@ -32,4 +39,33 @@ public class Xndirner {
         }
 
     }
-}
+    public static void DoZuygTver(){
+        int i = 1;
+        do {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }while(i <= 100);
+
+    }
+    public static void factorial(int number2){
+        int result = 1;
+        int i = 1;
+       while (i <= number2) {
+            result = result * i;
+            i++;
+        }
+        System.out.println(result);
+    }
+    public static void primeNumber(int i){
+
+        while (i <= 9){
+            System.out.println("this number is prime");
+        }
+        while(i > 9){
+            System.out.println("this number is not prime");
+        }
+    }
+    }
+
