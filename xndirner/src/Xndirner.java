db32afa22ca4c24fdfb3c805d21d50bab5bbe1a2
ah-2010 <138315547+ah-2010@users.noreply.github.com>
2023-11-11 12:01:24 +0400

    I did my 7 problems

--- xndirner/src/Xndirner.java
+++ xndirner/src/Xndirner.java
@@ -4,12 +4,9 @@ public class Xndirner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-       MekIcHarur();
-       DoMekIcHarur();
-        zuygTver();
-        DoZuygTver();
-        factorial(number);
-        primeNumber(number);
+        int number2 = sc.nextInt();
+
+        GCD(number, number2);
     }
 
     public static void MekIcHarur() {
@@ -39,33 +36,52 @@ public class Xndirner {
         }
 
     }
-    public static void DoZuygTver(){
+
+    public static void DoZuygTver() {
         int i = 1;
         do {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
             i++;
-        }while(i <= 100);
+        } while (i <= 100);
 
     }
-    public static void factorial(int number2){
+
+    public static void factorial(int number2) {
         int result = 1;
         int i = 1;
-       while (i <= number2) {
+        while (i <= number2) {
             result = result * i;
             i++;
         }
         System.out.println(result);
     }
-    public static void primeNumber(int i){
 
-        while (i <= 9){
+    public static void primeNumber(int i) {
+
+        while (i <= 9) {
             System.out.println("this number is prime");
         }
-        while(i > 9){
+        while (i > 9) {
             System.out.println("this number is not prime");
         }
     }
+
+    public static void GCD(int n1, int n2) {
+            int bajanarar = min(n1, n2);
+            while (!(n2 % bajanarar == 0 && n1 % bajanarar == 0)) {
+                bajanarar--;
+            }
+            System.out.println(bajanarar);
     }
+
+    public static int min(int a, int b) {
+        if (a > b) {
+            return b;
+        }
+        return a;
+    }
+}
+
 
