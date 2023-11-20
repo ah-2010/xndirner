cf1053d0817ee182849544038e24e2e0b13ffda9
ah-2010 <138315547+ah-2010@users.noreply.github.com>
2023-11-20 18:58:54 +0400

    I did my 7 problems

--- xndirner/src/Xndirner.java
+++ xndirner/src/Xndirner.java
@@ -4,12 +4,12 @@ public class Xndirner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int number2 = sc.nextInt();
+     //   int number2 = sc.nextInt();
 
-        GCD(number, number2);
+        primeNumber(number);
     }
 
-    public static void MekIcHarur() {
+    public static void mekIcHarur() {
         int i = 1;
         while (i <= 100) {
             System.out.println(i);
@@ -28,13 +28,14 @@ public class Xndirner {
 
     public static void zuygTver() {
         int i = 1;
+        int gumar = 0;
         while (i <= 100) {
             if (i % 2 == 0) {
-                System.out.println(i);
+                gumar = gumar + i;
             }
             i++;
         }
-
+        System.out.println(gumar);
     }
 
     public static void DoZuygTver() {
@@ -58,22 +59,27 @@ public class Xndirner {
         System.out.println(result);
     }
 
-    public static void primeNumber(int i) {
-
-        while (i <= 9) {
-            System.out.println("this number is prime");
+    public static void primeNumber(int number) {
+        int i = 2;
+        boolean trueOrfalse = true;
+        while (i <= number / 2) {
+            if (number % i == 0) {
+                trueOrfalse = false;
+            }
+            i++;
         }
-        while (i > 9) {
-            System.out.println("this number is not prime");
-        }
+        if (trueOrfalse)
+            System.out.println("ays tivy parz tiv e");
+        else
+            System.out.println("ays tivy baxadryal tiv e");
     }
 
     public static void GCD(int n1, int n2) {
-            int bajanarar = min(n1, n2);
-            while (!(n2 % bajanarar == 0 && n1 % bajanarar == 0)) {
-                bajanarar--;
-            }
-            System.out.println(bajanarar);
+        int bajanarar = min(n1, n2);
+        while (!(n2 % bajanarar == 0 && n1 % bajanarar == 0)) {
+            bajanarar--;
+        }
+        System.out.println(bajanarar);
     }
 
     public static int min(int a, int b) {
